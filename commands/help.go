37cc86aae33c69208c7d2233ba936fc3d84164e1
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-08-31 18:54:42 +0530

    update hel[p

--- commands/help.go
+++ commands/help.go
@@ -2,12 +2,12 @@ package commands
 
 func Help() string {
 	message := `Usage:
-	help           Show available commands and options.
-	login          Login to your account
-	signup         Create a new account
-	projects       List all projects in your account.
-	project        View and manage urls monitored in a specific project.
-	delete         Delete the prject specified
-	add            Add details to an existing or create a new project`
+	    help                         Show available commands and options.
+        login                        Login to your account
+        signup                       Create a new account
+        projects                     List all projects in your account.
+        project <project_name>       View and manage urls monitored in a specific project.
+        delete <project_name>        Delete the prject specified
+        add <config.st>              Add details to an existing or create a new project`
 	return message
 }
